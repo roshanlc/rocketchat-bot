7f7eb9bc121cf34afdc42f12962692788e3c1e8b
Roshan Lamichhane <roshan@jankaritech.com>
2023-01-23 17:07:51 +0545

    Initial setup

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/roshanlc/rocketchat-bot
+
+go 1.19
+
+require (
+	github.com/RocketChat/Rocket.Chat.Go.SDK v0.0.0-20221121042443-a3fd332d56d9
+	github.com/detached/gorocket v0.0.0-20170629192631-d44bbd3f26d2
+)
+
+require (
+	github.com/Jeffail/gabs v1.4.0 // indirect
+	github.com/gopackage/ddp v0.0.0-20170117053602-652027933df4 // indirect
+	github.com/sony/sonyflake v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20200904194848-62affa334b73 // indirect
+)
---
+++ main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"regexp"
+
+	"github.com/RocketChat/Rocket.Chat.Go.SDK/models"
+	"github.com/RocketChat/Rocket.Chat.Go.SDK/realtime"
+	"github.com/RocketChat/Rocket.Chat.Go.SDK/rest"
+)
+
+func main() {
+	server := url.URL{
+		Scheme: "http",
+		Host:   "localhost:3000",
+	}
+
+	realtimeClient, err := realtime.NewClient(&server, false)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	loginDetails := models.UserCredentials{
+		Email:    "bot@idk.com", //"haribdr@jankaritech.com",
+		Password: "pass",
+	}
+
+	userDetails, err := realtimeClient.Login(&loginDetails)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer realtimeClient.Close()
+
+	restClient := rest.NewClient(&server, false)
+
+	err = restClient.Login(&loginDetails)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Println("User details:\n", userDetails)
+
+	channelName := "try"
+
+	channelID, err := realtimeClient.GetChannelId(channelName)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	channelDetails := models.Channel{
+		ID: channelID,
+	}
+	fmt.Println("Channel Details:", channelDetails)
+
+	err = realtimeClient.JoinChannel(channelID)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// channel for listening to realtime msgs
+	msgs := make(chan models.Message)
+
+	err = realtimeClient.SubscribeToMessageStream(&channelDetails, msgs)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	reg1 := regexp.MustCompile(`(?mi)^.*\s*k.*c(h|hh|hhh)(a|aa|aaa)\s*$`)
+	whoamiRegExp := regexp.MustCompile(`(?mi)^.*\s*who\s*are\s*you\s*$`)
+
+	for msg := range msgs {
+		if reg1.MatchString(msg.Msg) {
+			tempMsg := models.Message{
+				RoomID: msg.RoomID,
+				Msg:    "K chaina sodhaana baru... list banara dinhu!!!!",
+			}
+
+			_, err := realtimeClient.SendMessage(&tempMsg)
+			if err != nil {
+				fmt.Println("Error while sending a message!")
+				continue
+			}
+		} else if whoamiRegExp.MatchString(msg.Msg) {
+			m := models.PostMessage{
+
+				//RoomID: msg.RoomID,
+				RoomID:  msg.RoomID,
+				Channel: msg.Channel,
+				// ParseUrls
+				// Alias
+				// Emoji
+				// Avatar
+				Attachments: []models.Attachment{{ImageURL: "https://nepstuff.com/wp-content/uploads/2020/07/ezgif.com-crop-2.gif"}},
+			}
+			restClient.PostMessage(&m)
+
+			// restClient.Send(&channelDetails)
+			// tempMsg := models.Message{
+			// 	RoomID: msg.RoomID,
+			// 	Msg:    "K chaina sodhaana baru... list banara dinhu!!!!",
+			// }
+
+			// _, err := realtimeClient.SendMessage(&tempMsg)
+			// if err != nil {
+			// 	fmt.Println("Error while sending a message!")
+			// 	continue
+			// }
+		}
+
+	}
+
+}
